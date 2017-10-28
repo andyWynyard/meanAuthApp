a66f920090bbadb442877fbaee67798700c73018
Andrew Wynyard <andrew.wynyard@gmail.com>
2017-10-28 11:25:43 +0200

    just some DB stuff and so on

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
   //database: 'mongodb://localhost:27017/meanauth',
-     database: 'mongodb://andyWynyard:andyWynyard@ds231715.mlab.com:31715/mean-auth-application',
+     database: 'mongodb://<USERNAME>:<PASSWORD>@ds231715.mlab.com:31715/mean-auth-application',
     secret: 'darth'
 }
